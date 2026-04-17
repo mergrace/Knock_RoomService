6bdf4d60025585fb98cafc50ccdf8e2794d14832
tavimbachihohwa9-a11y <tavimbachihohwa9@gmail.com>
2026-04-17 09:54:17 +0200

    update pause menu

---
+++ Knock_RoomService/Assets/Scripts (UI)/PauseMenu.cs
@@ -0,0 +1,31 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PauseMenu : MonoBehaviour
+{
+    [SerializeField] GameObject pauseMenu;
+
+    public void Pause ()
+    {
+        pauseMenu.SetActive(true);
+        Time.timeScale = 0f;
+    }
+
+    public void Resume()
+    {
+        pauseMenu.SetActive(false);
+        Time.timeScale = 1f;
+    }
+
+    public void Restart()
+    {
+        Time.timeScale = 1f;
+        SceneManager.LoadScene("Hallway");
+    }
+
+    public void Home(int sceneID)
+    {
+        Time.timeScale = 1f;
+        SceneManager.LoadScene(sceneID);
+    }
+}
