0f459356f41b299d8c8d5e836a6fae69d8fc9c97
mergrace <267859072+mergrace@users.noreply.github.com>
2026-04-19 20:00:49 +0200

    more commits

---
+++ Knock_RoomService/Assets/Scripts (UI)/floatupanddown.cs
@@ -0,0 +1,32 @@
+using UnityEngine;
+
+public class floatupanddown : MonoBehaviour
+{
+    public float amplitude = 2.8f;
+    public float frequency = 0.5f;
+
+
+    // Position Storage Variables
+    Vector3 posOffset = new Vector3();
+    Vector3 tempPos = new Vector3();
+
+    // Use this for initialization
+    void Start()
+    {
+        // Store the starting position & rotation of the object
+        posOffset = transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        // Float up/down with a Sin()
+        tempPos = posOffset;
+        tempPos.y += Mathf.Sin(Time.fixedTime * Mathf.PI * frequency) * amplitude;
+
+        transform.position = tempPos;
+    }
+
+
+}
