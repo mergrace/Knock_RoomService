a7de4bc17a1065f5d01982f761eae33783ef2e5c
mergrace <267859072+mergrace@users.noreply.github.com>
2026-04-20 23:46:53 +0200

    Added button to change scenes
    Room 1 transition
    Hallway transition

---
+++ Knock_RoomService/Assets/Scripts/ChangesScenesHallway.cs
@@ -0,0 +1,13 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ChangesScenesHallway : MonoBehaviour
+{
+    public void GoToHallway()
+    {
+        SceneManager.LoadScene("Hallway");
+    }
+
+
+
+}
