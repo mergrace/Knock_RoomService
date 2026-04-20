fc3fb6ed0ee81afb9966f600b8bff8e04d4aacd6
mergrace <267859072+mergrace@users.noreply.github.com>
2026-04-21 00:21:31 +0200

    Added code to hallway button

--- Knock_RoomService/Assets/Scripts/UI/SceneChangerHallway.cs
+++ Knock_RoomService/Assets/Scripts/UI/SceneChangerHallway.cs
@@ -1,16 +1,12 @@
 using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
 
 public class SceneChangerHallway : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    public void GoToRoom1()
     {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
+        SceneManager.LoadScene("Room 1");
     }
 }
