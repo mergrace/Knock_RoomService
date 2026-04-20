c542a248675266ad27491cc110a00f902c31c438
mergrace <267859072+mergrace@users.noreply.github.com>
2026-04-20 02:46:35 +0200

    Added Floater script

---
+++ Knock_RoomService/Assets/Scripts/Floatupanddown.cs
@@ -0,0 +1,28 @@
+using UnityEngine;
+
+
+public class Floatupanddown : MonoBehaviour
+{
+    public float amplitude = 0.5f;
+    public float frequency = 1f;
+
+    
+    Vector3 posOffset = new Vector3();
+    Vector3 tempPos = new Vector3();
+
+    void Start()
+    {
+        // Store the starting position 
+        posOffset = transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Float up/down with a Sin()
+        tempPos = posOffset;
+        tempPos.y += Mathf.Sin(Time.fixedTime * Mathf.PI * frequency) * amplitude;
+
+        transform.position = tempPos;
+    }
+}
