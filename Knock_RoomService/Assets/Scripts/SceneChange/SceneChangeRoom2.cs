7481d0b13ad56d6f3d24ea53ddaf6f810de055eb
FaeUncivilizedWorld <oguchifavour0430@gmail.com>
2026-05-06 22:40:35 +0200

    update in room 2 and hallway
    added interactive buttons (scene change) to room 1 and hallway
    i changed the bullets to be the actual images in the inventory and fixed the camera to not show outside of the scene in game tab.

---
+++ Knock_RoomService/Assets/Scripts/SceneChange/SceneChangeRoom2.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneChangeRoom2 : MonoBehaviour
+{
+    public void GoToRoom2()
+    {
+        SceneManager.LoadScene("Room 2");
+    }
+}
