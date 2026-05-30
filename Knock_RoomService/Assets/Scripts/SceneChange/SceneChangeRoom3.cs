a149c64735e1271dba4ee684664ca0c1f55ce77e
FaeUncivilizedWorld <oguchifavour0430@gmail.com>
2026-05-30 14:42:31 +0200

    updates
    in hallway and room 3
    added a button in hallway and changed the fonts

---
+++ Knock_RoomService/Assets/Scripts/SceneChange/SceneChangeRoom3.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneChangeRoom3 : MonoBehaviour
+{
+    public void GoToRoom3()
+    {
+        SceneManager.LoadScene("Room 3");
+    }
+}
